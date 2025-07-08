99e8018968ea398994fe0f220df87b5260d34d0a
gwanoove <bestmd0320@gmail.com>
2025-07-08 21:22:32 +0900

    Update PlayerDetailView.swift

--- swiftui-project-team4/BaseaballQuiz/BaseaballQuiz/PlayerDetailView.swift
+++ swiftui-project-team4/BaseaballQuiz/BaseaballQuiz/PlayerDetailView.swift
@@ -6,139 +6,251 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct PlayerDetailView: View {
+    var isPicher: Bool = false
+    
     
     var body: some View {
         @State var position: String = ""
         
-        
         NavigationStack {
-            Form {
-                Section(header: Text("타자").bold() .font(.largeTitle)) {
-                    
-                    
-                    Group {
-                        HStack {
-                            AsyncImage(url: URL(string: "https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/53123.jpg")) { image in
-                                image.resizable()
-                            } placeholder: {
-                                ProgressView()
-                            }
-                            .frame(width: 70, height: 100)
-                            
-                            Grid {
-                                GridRow {
-                                    Text("이   름 :")
-                                    Text("오스틴")
-                                }
-                                
-                                GridRow {
-                                    Text("포지션 :")
-                                    Text("내야수(우투우타)")
-                                }
-                                
-                                GridRow {
-                                    Text("출   생 :")
-                                    Text("1999-07-02")
-                                }
-                                GridRow {
-                                    Text("체   격 :")
-                                    Text("183cm, 85kg")
-                                }
-                            }
-                            .padding(1)
-                        }
-                        
-                        
-                    }
-                    
-                    HStack {
-                        Text("홈런")
-                        Text("타수")
-                        Text("안타")
-                        Text("타점")
-                        Text("득점")
-                        Text("도루")
-                        Text("출루율")
-                    }
-                    .font(.headline)
-                    
-                    HStack {
-                        Text("  0  ")
-                        Text("  0  ")
-                        Text("  0  ")
-                        Text("  0  ")
-                        Text("   0   ")
-                        Text("  0  ")
-                        Text("    0   ")
-                        
-                        
-                        }
-                    }
-                Section(header: Text("투수").bold() .font(.largeTitle)) {
-                    
-                    
-                    Group {
-                        HStack {
-                            AsyncImage(url: URL(string: "https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/69159.jpg")) { image in
-                                image.resizable()
-                            } placeholder: {
-                                ProgressView()
-                            }
-                            .frame(width: 70, height: 100)
-                            Grid {
-                                GridRow {
-                                    Text("이   름 :")
-                                    Text("정우영")
-                                }
-                                
-                                GridRow {
-                                    Text("포지션 :")
-                                    Text("투수")
-                                }
-                                
-                                GridRow {
-                                    Text("출   생 :")
-                                    Text("1999-09-19")
-                                }
-                                GridRow {
-                                    Text("체   격 :")
-                                    Text("193cm, 99kg")
+            ZStack {
+                Form {
+                    if isPicher {
+                        Section(header: Text("타자").kerning(4) .bold() .font(.largeTitle)) {
+                            Group {
+                                HStack {
+                                    AsyncImage(url: URL(string: "https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/53123.jpg")) { image in
+                                        image.resizable()
+                                    } placeholder: {
+                                        ProgressView()
+                                    }
+                                    .frame(width: 80, height: 100)
+                                    .padding(3)
+                                    
+                                    
+                                    VStack (alignment: .leading) {
+                                        HStack {
+                                            Text("이  름 :")
+                                                .kerning(1)
+                                            
+                                            Text("오스틴")
+                                                .kerning(2)
+                                            
+                                        }
+                                        .font(.title2)
+                                        .padding(0.5)
+                                        HStack {
+                                            Text("포지션 :")
+                                                .kerning(2.5)
+                                            
+                                            Text("내야수(우투우타)")
+                                                .kerning(2)
+                                            
+                                        }
+                                        
+                                        HStack {
+                                            Text("출   생 :")
+                                                .kerning(2)
+                                            
+                                            Text("1999-07-02")
+                                                .kerning(1)
+                                        }
+                                        HStack {
+                                            Text("체   격 :")
+                                                .kerning(2)
+                                            
+                                            Text("183cm, 85kg")
+                                                .kerning(1)
+                                        }
+                                    }
+                                    .padding(2)
                                 }
                             }
-                            .padding(1)
+                            ScrollView(.horizontal) {
+                                HStack {
+                                    VStack {
+                                        Text("타율")
+                                            .kerning(2)
+                                        
+                                        Text("0")
+                                    }
+                                    VStack {
+                                        Text("홈런")
+                                            .kerning(2)
+                                        
+                                        Text("0")
+                                    }
+                                    VStack {
+                                        Text("타수")
+                                            .kerning(2)
+                                        
+                                        Text("0")
+                                    }
+                                    VStack {
+                                        Text("안타")
+                                            .kerning(2)
+                                        
+                                        Text("0")
+                                    }
+                                    VStack {
+                                        Text("타점")
+                                            .kerning(2)
+                                        
+                                        Text("0")
+                                    }
+                                    VStack {
+                                        Text("득점")
+                                            .kerning(2)
+                                        
+                                        Text("0")
+                                    }
+                                    VStack {
+                                        Text("도루")
+                                            .kerning(2)
+                                        
+                                        Text("0")
+                                    }
+                                    VStack {
+                                        Text("출루율")
+                                            .kerning(2)
+                                        
+                                        Text("0")
+                                    }
+                                    VStack {
+                                        Text("OPS")
+                                            .kerning(2)
+                                        
+                                        Text("0")
+                                    }
+                                }
+                            }
                         }
+                    } else {
                         
-                        
-                    }
-                    HStack {
-                        Text("평균자책점")
-                        Text(" 승 ")
-                        Text(" 패 ")
-                        Text(" 이닝 ")
-                        Text(" 피안타 ")
-                        Text("볼넷")
-                    }
-                    .font(.headline)
-                    
-                    HStack {
-                        Text("       0      ")
-                        Text("  0  ")
-                        Text("  0  ")
-                        Text("  0  ")
-                        Text("      0     ")
-                        Text("  0  ")
-                        
-                        
+                        Section(header: Text("투수").kerning(4) .bold() .font(.largeTitle)) {
+                            Group {
+                                HStack {
+                                    AsyncImage(url: URL(string: "https://6ptotvmi5753.edge.naverncp.com/KBO_IMAGE/person/middle/2025/53123.jpg")) { image in
+                                        image.resizable()
+                                    } placeholder: {
+                                        ProgressView()
+                                    }
+                                    .frame(width: 80, height: 100)
+                                    .padding(3)
+                                    
+                                    
+                                    VStack (alignment: .leading) {
+                                        HStack {
+                                            Text("이  름 :")
+                                                .kerning(1)
+                                            
+                                            Text("오스틴")
+                                                .kerning(2)
+                                            
+                                        }
+                                        .font(.title2)
+                                        .padding(0.5)
+                                        HStack {
+                                            Text("포지션 :")
+                                                .kerning(2.5)
+                                            
+                                            Text("내야수(우투우타)")
+                                                .kerning(2)
+                                            
+                                        }
+                                        
+                                        HStack {
+                                            Text("출   생 :")
+                                                .kerning(2)
+                                            
+                                            Text("1999-07-02")
+                                                .kerning(1)
+                                        }
+                                        HStack {
+                                            Text("체   격 :")
+                                                .kerning(2)
+                                            
+                                            Text("183cm, 85kg")
+                                                .kerning(1)
+                                        }
+                                    }
+                                    .padding(2)
+                                }
+                            }
+                            ScrollView(.horizontal) {
+                                HStack {
+                                    VStack {
+                                        Text("타율")
+                                            .kerning(2)
+                                        
+                                        Text("0")
+                                    }
+                                    VStack {
+                                        Text("홈런")
+                                            .kerning(2)
+                                        
+                                        Text("0")
+                                    }
+                                    VStack {
+                                        Text("타수")
+                                            .kerning(2)
+                                        
+                                        Text("0")
+                                    }
+                                    VStack {
+                                        Text("안타")
+                                            .kerning(2)
+                                        
+                                        Text("0")
+                                    }
+                                    VStack {
+                                        Text("타점")
+                                            .kerning(2)
+                                        
+                                        Text("0")
+                                    }
+                                    VStack {
+                                        Text("득점")
+                                            .kerning(2)
+                                        
+                                        Text("0")
+                                    }
+                                    VStack {
+                                        Text("도루")
+                                            .kerning(2)
+                                        
+                                        Text("0")
+                                    }
+                                    VStack {
+                                        Text("출루율")
+                                            .kerning(2)
+                                        
+                                        Text("0")
+                                    }
+                                    VStack {
+                                        Text("OPS")
+                                            .kerning(2)
+                                        
+                                        Text("0")
+                                    }
+                                }
+                            }
                         }
                     }
+                }
+                    .scrollContentBackground(.hidden)
+                    .background(Color.clear)
                 }
             }
         }
     }
-
-
+    
 #Preview {
     PlayerDetailView()
 }
+
+
+
